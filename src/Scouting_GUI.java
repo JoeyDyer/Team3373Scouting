26b3f9b914e3cc707994bc3673fa0c93b4aa264c
alexanderiasso <alexanderiasso@10.0.1.59>
2015-01-31 19:05:42 -0500

    Scouting Imporvements

--- src/Scouting_GUI.java
+++ src/Scouting_GUI.java
@@ -77,6 +77,23 @@ public class Scouting_GUI {
 	GridBagConstraints gbc_txtEnterPointsEarned_3 = new GridBagConstraints();
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
+	private JTextField txtTest;
+	private JButton button;
+	private JButton button_1;
+	private JButton button_2;
+	private JButton button_3;
+	private JButton button_4;
+	private JButton button_5;
+	private JButton button_6;
+	private JButton button_7;
+	private JButton button_8;
+	private JButton button_9;
+	private JButton button_10;
+	private JButton button_11;
+	private JButton button_12;
+	private JButton button_13;
+	private JButton button_14;
+	private JButton button_15;
 	
 
 	/**
@@ -109,14 +126,17 @@ public class Scouting_GUI {
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		frame.getContentPane().setBackground(new Color(255, 20, 147));
-		frame.setBounds(100, 100, 627, 602);
+		frame.setBounds(100, 100, 671, 602);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{1, 114, 267, 71, 92, 0};
-		gridBagLayout.rowHeights = new int[]{14, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 48, 23, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{1, 153, 230, 0, 71, 92, 0};
+		gridBagLayout.rowHeights = new int[]{14, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 48, 23, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
+		
+		
+		
 		
 		lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(null);
@@ -147,7 +167,7 @@ public class Scouting_GUI {
 		lblName = new JLabel("Name");
 		GridBagConstraints gbc_lblName = new GridBagConstraints();
 		gbc_lblName.insets = new Insets(0, 0, 5, 0);
-		gbc_lblName.gridx = 4;
+		gbc_lblName.gridx = 5;
 		gbc_lblName.gridy = 1;
 		frame.getContentPane().add(lblName, gbc_lblName);
 		
@@ -165,13 +185,14 @@ public class Scouting_GUI {
 		lblMatch = new JLabel("Match");
 		GridBagConstraints gbc_lblMatch = new GridBagConstraints();
 		gbc_lblMatch.insets = new Insets(0, 0, 5, 0);
-		gbc_lblMatch.gridx = 4;
+		gbc_lblMatch.gridx = 5;
 		gbc_lblMatch.gridy = 2;
 		frame.getContentPane().add(lblMatch, gbc_lblMatch);
 		
 		//HandlerClass handler = new HandlerClass(txtEnterTargetTeam, txtEnterMatchNumber, txtEnterPointsEarned, txtEnterMaximumHeight, txtEnterPointsScored, txtEnterPointsEarned_1, txtEnterMaximumHeight_1, txtEnterPointsEarned_2, txtEnterNumberOf, txtEnterPointsEarned_3, txtEnterAdditionalNotes, txtEnterYourName);
 		
 		txtEnterTargetTeam = new JTextField();
+		txtEnterTargetTeam.setEditable(true);
 		txtEnterTargetTeam.setText("Enter Target Team Number");
 		GridBagConstraints gbc_txtEnterTargetTeam_1 = new GridBagConstraints();
 		gbc_txtEnterTargetTeam_1.fill = GridBagConstraints.HORIZONTAL;
@@ -185,12 +206,23 @@ public class Scouting_GUI {
 		lblTeamNumber = new JLabel("Team Number");
 		GridBagConstraints gbc_lblTeamNumber = new GridBagConstraints();
 		gbc_lblTeamNumber.insets = new Insets(0, 0, 5, 0);
-		gbc_lblTeamNumber.gridx = 4;
+		gbc_lblTeamNumber.gridx = 5;
 		gbc_lblTeamNumber.gridy = 3;
 		frame.getContentPane().add(lblTeamNumber, gbc_lblTeamNumber);
 		
+		int vara = 0;
+		int varb = 0;
+		int varc = 0;
+		int vard = 0;
+		int vare = 0;
+		int varf = 0;
+		int varg = 0;
+		int varh = 0;
+
+		
 		txtEnterPointsScored = new JTextField();
-		txtEnterPointsScored.setText("Enter Points Scored in Autonomous");
+		txtEnterPointsScored.setEditable(false);
+		txtEnterPointsScored.setText("" + vara);
 		GridBagConstraints gbc_txtEnterPointsScored_1 = new GridBagConstraints();
 		gbc_txtEnterPointsScored_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterPointsScored_1.insets = new Insets(0, 0, 5, 5);
@@ -200,15 +232,34 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterPointsScored, gbc_txtEnterPointsScored_1);
 		txtEnterPointsScored.setColumns(10);
 		
+		button = new JButton("+");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 5, 5);
+		gbc_button.gridx = 3;
+		gbc_button.gridy = 4;
+		frame.getContentPane().add(button, gbc_button);
+		
+		button_1 = new JButton("-");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.insets = new Insets(0, 0, 5, 5);
+		gbc_button_1.gridx = 4;
+		gbc_button_1.gridy = 4;
+		frame.getContentPane().add(button_1, gbc_button_1);
+		
 		lblAutonomousPoints = new JLabel("Autonomous Points");
 		GridBagConstraints gbc_lblAutonomousPoints = new GridBagConstraints();
 		gbc_lblAutonomousPoints.insets = new Insets(0, 0, 5, 0);
-		gbc_lblAutonomousPoints.gridx = 4;
+		gbc_lblAutonomousPoints.gridx = 5;
 		gbc_lblAutonomousPoints.gridy = 4;
 		frame.getContentPane().add(lblAutonomousPoints, gbc_lblAutonomousPoints);
 		
 		txtEnterPointsEarned = new JTextField();
-		txtEnterPointsEarned.setText("Enter Points Earned by Tote Stacking");
+		txtEnterPointsEarned.setEditable(false);
+		txtEnterPointsEarned.setText("" + varb);
 		GridBagConstraints gbc_txtEnterPointsEarned_4 = new GridBagConstraints();
 		gbc_txtEnterPointsEarned_4.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterPointsEarned_4.insets = new Insets(0, 0, 5, 5);
@@ -218,15 +269,30 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterPointsEarned, gbc_txtEnterPointsEarned_4);
 		txtEnterPointsEarned.setColumns(10);
 		
+		button_2 = new JButton("+");
+		GridBagConstraints gbc_button_2 = new GridBagConstraints();
+		gbc_button_2.insets = new Insets(0, 0, 5, 5);
+		gbc_button_2.gridx = 3;
+		gbc_button_2.gridy = 5;
+		frame.getContentPane().add(button_2, gbc_button_2);
+		
+		button_3 = new JButton("-");
+		GridBagConstraints gbc_button_3 = new GridBagConstraints();
+		gbc_button_3.insets = new Insets(0, 0, 5, 5);
+		gbc_button_3.gridx = 4;
+		gbc_button_3.gridy = 5;
+		frame.getContentPane().add(button_3, gbc_button_3);
+		
 		lblTotePoints = new JLabel("Tote Points");
 		GridBagConstraints gbc_lblTotePoints = new GridBagConstraints();
 		gbc_lblTotePoints.insets = new Insets(0, 0, 5, 0);
-		gbc_lblTotePoints.gridx = 4;
+		gbc_lblTotePoints.gridx = 5;
 		gbc_lblTotePoints.gridy = 5;
 		frame.getContentPane().add(lblTotePoints, gbc_lblTotePoints);
 		
 		txtEnterMaximumHeight = new JTextField();
-		txtEnterMaximumHeight.setText("Enter Maximum Height of Totes Stacked");
+		txtEnterMaximumHeight.setEditable(false);
+		txtEnterMaximumHeight.setText("" + varc);
 		GridBagConstraints gbc_txtEnterMaximumHeight_2 = new GridBagConstraints();
 		gbc_txtEnterMaximumHeight_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterMaximumHeight_2.insets = new Insets(0, 0, 5, 5);
@@ -236,15 +302,30 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterMaximumHeight, gbc_txtEnterMaximumHeight_2);
 		txtEnterMaximumHeight.setColumns(10);
 		
+		button_4 = new JButton("+");
+		GridBagConstraints gbc_button_4 = new GridBagConstraints();
+		gbc_button_4.insets = new Insets(0, 0, 5, 5);
+		gbc_button_4.gridx = 3;
+		gbc_button_4.gridy = 6;
+		frame.getContentPane().add(button_4, gbc_button_4);
+		
+		button_5 = new JButton("-");
+		GridBagConstraints gbc_button_5 = new GridBagConstraints();
+		gbc_button_5.insets = new Insets(0, 0, 5, 5);
+		gbc_button_5.gridx = 4;
+		gbc_button_5.gridy = 6;
+		frame.getContentPane().add(button_5, gbc_button_5);
+		
 		lblToteHeight = new JLabel("Tote Height");
 		GridBagConstraints gbc_lblToteHeight = new GridBagConstraints();
 		gbc_lblToteHeight.insets = new Insets(0, 0, 5, 0);
-		gbc_lblToteHeight.gridx = 4;
+		gbc_lblToteHeight.gridx = 5;
 		gbc_lblToteHeight.gridy = 6;
 		frame.getContentPane().add(lblToteHeight, gbc_lblToteHeight);
 		
 		txtEnterPointsEarned_1 = new JTextField();
-		txtEnterPointsEarned_1.setText("Enter Points Earned with Cans");
+		txtEnterPointsEarned_1.setEditable(false);
+		txtEnterPointsEarned_1.setText("" + vard);
 		GridBagConstraints gbc_txtEnterPointsEarned_1_1 = new GridBagConstraints();
 		gbc_txtEnterPointsEarned_1_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterPointsEarned_1_1.insets = new Insets(0, 0, 5, 5);
@@ -254,15 +335,30 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterPointsEarned_1, gbc_txtEnterPointsEarned_1_1);
 		txtEnterPointsEarned_1.setColumns(10);
 		
+		button_6 = new JButton("+");
+		GridBagConstraints gbc_button_6 = new GridBagConstraints();
+		gbc_button_6.insets = new Insets(0, 0, 5, 5);
+		gbc_button_6.gridx = 3;
+		gbc_button_6.gridy = 7;
+		frame.getContentPane().add(button_6, gbc_button_6);
+		
+		button_7 = new JButton("-");
+		GridBagConstraints gbc_button_7 = new GridBagConstraints();
+		gbc_button_7.insets = new Insets(0, 0, 5, 5);
+		gbc_button_7.gridx = 4;
+		gbc_button_7.gridy = 7;
+		frame.getContentPane().add(button_7, gbc_button_7);
+		
 		lblCanPoints = new JLabel("Can Points");
 		GridBagConstraints gbc_lblCanPoints = new GridBagConstraints();
 		gbc_lblCanPoints.insets = new Insets(0, 0, 5, 0);
-		gbc_lblCanPoints.gridx = 4;
+		gbc_lblCanPoints.gridx = 5;
 		gbc_lblCanPoints.gridy = 7;
 		frame.getContentPane().add(lblCanPoints, gbc_lblCanPoints);
 		
 		txtEnterMaximumHeight_1 = new JTextField();
-		txtEnterMaximumHeight_1.setText("Enter Maximum Height of Stacked Cans");
+		txtEnterMaximumHeight_1.setEditable(false);
+		txtEnterMaximumHeight_1.setText("" + vare);
 		GridBagConstraints gbc_txtEnterMaximumHeight_1_1 = new GridBagConstraints();
 		gbc_txtEnterMaximumHeight_1_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterMaximumHeight_1_1.insets = new Insets(0, 0, 5, 5);
@@ -272,15 +368,30 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterMaximumHeight_1, gbc_txtEnterMaximumHeight_1_1);
 		txtEnterMaximumHeight_1.setColumns(10);
 		
+		button_8 = new JButton("+");
+		GridBagConstraints gbc_button_8 = new GridBagConstraints();
+		gbc_button_8.insets = new Insets(0, 0, 5, 5);
+		gbc_button_8.gridx = 3;
+		gbc_button_8.gridy = 8;
+		frame.getContentPane().add(button_8, gbc_button_8);
+		
+		button_9 = new JButton("-");
+		GridBagConstraints gbc_button_9 = new GridBagConstraints();
+		gbc_button_9.insets = new Insets(0, 0, 5, 5);
+		gbc_button_9.gridx = 4;
+		gbc_button_9.gridy = 8;
+		frame.getContentPane().add(button_9, gbc_button_9);
+		
 		lblCanHeight = new JLabel("Can Height");
 		GridBagConstraints gbc_lblCanHeight = new GridBagConstraints();
 		gbc_lblCanHeight.insets = new Insets(0, 0, 5, 0);
-		gbc_lblCanHeight.gridx = 4;
+		gbc_lblCanHeight.gridx = 5;
 		gbc_lblCanHeight.gridy = 8;
 		frame.getContentPane().add(lblCanHeight, gbc_lblCanHeight);
 		
 		txtEnterPointsEarned_2 = new JTextField();
-		txtEnterPointsEarned_2.setText("Enter Points Earned by Litter");
+		txtEnterPointsEarned_2.setEditable(false);
+		txtEnterPointsEarned_2.setText("" + varf);
 		GridBagConstraints gbc_txtEnterPointsEarned_2_1 = new GridBagConstraints();
 		gbc_txtEnterPointsEarned_2_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterPointsEarned_2_1.insets = new Insets(0, 0, 5, 5);
@@ -290,16 +401,31 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterPointsEarned_2, gbc_txtEnterPointsEarned_2_1);
 		txtEnterPointsEarned_2.setColumns(10);
 		
+		button_10 = new JButton("+");
+		GridBagConstraints gbc_button_10 = new GridBagConstraints();
+		gbc_button_10.insets = new Insets(0, 0, 5, 5);
+		gbc_button_10.gridx = 3;
+		gbc_button_10.gridy = 9;
+		frame.getContentPane().add(button_10, gbc_button_10);
+		
+		button_11 = new JButton("-");
+		GridBagConstraints gbc_button_11 = new GridBagConstraints();
+		gbc_button_11.insets = new Insets(0, 0, 5, 5);
+		gbc_button_11.gridx = 4;
+		gbc_button_11.gridy = 9;
+		frame.getContentPane().add(button_11, gbc_button_11);
+		
 		lblLitterPoints = new JLabel("Litter Points");
 		GridBagConstraints gbc_lblLitterPoints = new GridBagConstraints();
 		gbc_lblLitterPoints.insets = new Insets(0, 0, 5, 0);
-		gbc_lblLitterPoints.gridx = 4;
+		gbc_lblLitterPoints.gridx = 5;
 		gbc_lblLitterPoints.gridy = 9;
 		frame.getContentPane().add(lblLitterPoints, gbc_lblLitterPoints);
 		
 		txtEnterNumberOf = new JTextField();
+		txtEnterNumberOf.setEditable(false);
 		txtEnterNumberOf.setBackground(Color.WHITE);
-		txtEnterNumberOf.setText("Enter Number of Totes Litter is Scored On");
+		txtEnterNumberOf.setText("" + varg);
 		GridBagConstraints gbc_txtEnterNumberOf_1 = new GridBagConstraints();
 		gbc_txtEnterNumberOf_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterNumberOf_1.insets = new Insets(0, 0, 5, 5);
@@ -309,16 +435,31 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterNumberOf, gbc_txtEnterNumberOf_1);
 		txtEnterNumberOf.setColumns(10);
 		
+		button_12 = new JButton("+");
+		GridBagConstraints gbc_button_12 = new GridBagConstraints();
+		gbc_button_12.insets = new Insets(0, 0, 5, 5);
+		gbc_button_12.gridx = 3;
+		gbc_button_12.gridy = 10;
+		frame.getContentPane().add(button_12, gbc_button_12);
+		
+		button_13 = new JButton("-");
+		GridBagConstraints gbc_button_13 = new GridBagConstraints();
+		gbc_button_13.insets = new Insets(0, 0, 5, 5);
+		gbc_button_13.gridx = 4;
+		gbc_button_13.gridy = 10;
+		frame.getContentPane().add(button_13, gbc_button_13);
+		
 		lblLitterHeight = new JLabel("Litter Height");
 		GridBagConstraints gbc_lblLitterHeight = new GridBagConstraints();
 		gbc_lblLitterHeight.insets = new Insets(0, 0, 5, 0);
-		gbc_lblLitterHeight.gridx = 4;
+		gbc_lblLitterHeight.gridx = 5;
 		gbc_lblLitterHeight.gridy = 10;
 		frame.getContentPane().add(lblLitterHeight, gbc_lblLitterHeight);
 		
 		txtEnterPointsEarned_3 = new JTextField();
+		txtEnterPointsEarned_3.setEditable(false);
 		txtEnterPointsEarned_3.setBackground(Color.WHITE);
-		txtEnterPointsEarned_3.setText("Enter Points Earned by Coopertition");
+		txtEnterPointsEarned_3.setText("" + varh);
 		GridBagConstraints gbc_txtEnterPointsEarned_3_1 = new GridBagConstraints();
 		gbc_txtEnterPointsEarned_3_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtEnterPointsEarned_3_1.insets = new Insets(0, 0, 5, 5);
@@ -328,14 +469,29 @@ public class Scouting_GUI {
 		frame.getContentPane().add(txtEnterPointsEarned_3, gbc_txtEnterPointsEarned_3_1);
 		txtEnterPointsEarned_3.setColumns(10);
 		
+		button_14 = new JButton("+");
+		GridBagConstraints gbc_button_14 = new GridBagConstraints();
+		gbc_button_14.insets = new Insets(0, 0, 5, 5);
+		gbc_button_14.gridx = 3;
+		gbc_button_14.gridy = 11;
+		frame.getContentPane().add(button_14, gbc_button_14);
+		
+		button_15 = new JButton("-");
+		GridBagConstraints gbc_button_15 = new GridBagConstraints();
+		gbc_button_15.insets = new Insets(0, 0, 5, 5);
+		gbc_button_15.gridx = 4;
+		gbc_button_15.gridy = 11;
+		frame.getContentPane().add(button_15, gbc_button_15);
+		
 		lblCoopertitionPoints = new JLabel("Coopertition Points");
 		GridBagConstraints gbc_lblCoopertitionPoints = new GridBagConstraints();
 		gbc_lblCoopertitionPoints.insets = new Insets(0, 0, 5, 0);
-		gbc_lblCoopertitionPoints.gridx = 4;
+		gbc_lblCoopertitionPoints.gridx = 5;
 		gbc_lblCoopertitionPoints.gridy = 11;
 		frame.getContentPane().add(lblCoopertitionPoints, gbc_lblCoopertitionPoints);
 		
 		txtEnterAdditionalNotes = new JTextField();
+		txtEnterAdditionalNotes.setEditable(true);
 		txtEnterAdditionalNotes.setBackground(Color.WHITE);
 		txtEnterAdditionalNotes.setText("Enter Additional Notes Here");
 		GridBagConstraints gbc_txtEnterAdditionalNotes_1 = new GridBagConstraints();
@@ -350,7 +506,7 @@ public class Scouting_GUI {
 		lblAdditionalNotes = new JLabel("Additional Notes");
 		GridBagConstraints gbc_lblAdditionalNotes = new GridBagConstraints();
 		gbc_lblAdditionalNotes.insets = new Insets(0, 0, 5, 0);
-		gbc_lblAdditionalNotes.gridx = 4;
+		gbc_lblAdditionalNotes.gridx = 5;
 		gbc_lblAdditionalNotes.gridy = 12;
 		frame.getContentPane().add(lblAdditionalNotes, gbc_lblAdditionalNotes);
 		
@@ -359,8 +515,8 @@ public class Scouting_GUI {
 		lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon("rsz_robo_pink_burned.png"));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 4;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 5;
 		gbc_lblNewLabel_1.gridy = 13;
 		frame.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
@@ -370,7 +526,7 @@ public class Scouting_GUI {
 		btnSubmit.setBackground(Color.WHITE);
 		GridBagConstraints gbc_btnSubmit = new GridBagConstraints();
 		gbc_btnSubmit.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnSubmit.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSubmit.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSubmit.gridwidth = 3;
 		gbc_btnSubmit.gridx = 0;
 		gbc_btnSubmit.gridy = 14;
