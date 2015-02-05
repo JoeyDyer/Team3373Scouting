229839a2844b7798affd3452bcbfd3c869dce9e7
alexanderiasso <alexanderiasso@10.0.1.59>
2015-02-05 10:08:21 -0500

    Changed naming of scouting files

--- src/Scouting_Compiler.java
+++ src/Scouting_Compiler.java
@@ -3,7 +3,11 @@ public class Scouting_Compiler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// We need a way to know the length of the team #
+		//We need to read the team number and find matching numbers
+		//We need to read those files and pull variables from the text (Using a method similar to what we used to read the JTextFields should work)
+		
+	
 	}
 
 }
