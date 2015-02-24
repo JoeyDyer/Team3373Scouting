549b8d486f2414c99dd24d67fb0f109db792ce66
alexanderiasso <alexanderiasso@172.16.3.132>
2015-02-24 15:42:37 -0500

    Added tests

--- src/textfiles/Deafh.java
+++ src/textfiles/Deafh.java
@@ -7,7 +7,7 @@ public class Deafh {
 	public static int money = 0;
 	public static void main(String[] args) {
 
-		JOptionPane.showMessageDialog(null, "WOWOWOWOWOWOWOWWOWOWOWOWOWOWOWWOWOWOWOWOWOWOWOWOWOWWOWOWOWOWOWOWOWOWOWOWOWOWOWOWWO");
+		JOptionPane.showMessageDialog(null, "I hope you're ready!");
 		
 		 if(money == 1){
 			 System.out.println("You are broke! YOU NEED TO GO DIE! GET HIT BY A BUS! AND A TRAIN!!!! AT THE SAME TIME!");
